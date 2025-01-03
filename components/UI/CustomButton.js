5c1ca2fa97d6bd08e314f9d6f3ff2b7415e05d48
Vachek-hooves <vachek0602@gmail.com>
2025-01-03 20:11:06 +0200

    update btn styles

--- components/UI/CustomButton.js
+++ components/UI/CustomButton.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import {StyleSheet, Text, TouchableOpacity} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 
-const CustomButton = ({ title, onPress, variant = 'secondary' }) => {
+const CustomButton = ({title, onPress, variant = 'secondary'}) => {
   const isPlayButton = variant === 'primary';
 
   return (
     <LinearGradient
-      colors={isPlayButton ? ['#FFEA9E', '#FCF8EA'] : ['rgba(128, 128, 128, 0.3)', 'rgba(128, 128, 128, 0.3)']}
+      colors={
+        isPlayButton
+          ? ['#FFEA9E', '#FCF8EA']
+          : ['rgba(36, 36, 36, 1)', 'rgba(36, 36, 36, 1)']
+      }
       style={[styles.button, isPlayButton && styles.buttonShadow]}
       start={{x: 0, y: 0}}
       end={{x: 1, y: 1}}>
-      <TouchableOpacity
-        onPress={onPress}
-        style={styles.touchable}>
-        <Text style={[
-          styles.buttonText,
-          isPlayButton && styles.primaryButtonText
-        ]}>
+      <TouchableOpacity onPress={onPress} style={styles.touchable}>
+        <Text
+          style={[styles.buttonText, isPlayButton && styles.primaryButtonText]}>
           {title}
         </Text>
       </TouchableOpacity>
@@ -30,6 +30,14 @@ const styles = StyleSheet.create({
     borderRadius: 25,
     marginVertical: 8,
     width: '100%',
+    shadowColor: '#FCF8EA',
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 0.9,
+    shadowRadius: 4,
+    elevation: 10,
   },
   buttonShadow: {
     shadowColor: '#FCF8EA',
@@ -55,4 +63,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default CustomButton; 
+export default CustomButton;
