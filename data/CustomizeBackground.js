eeaf3b68e47556f7883682afc7d79c96d7606104
Vachek-hooves <vachek0602@gmail.com>
2025-01-03 21:30:47 +0200

    add crowns images

---
+++ data/CustomizeBackground.js
@@ -0,0 +1,6 @@
+export const background = [
+  {id: 1, bgColor: '#000'},
+  {id: 2, bgColor: '#FCF8EA'},
+  {id: 3, bgColor: '#989DFF'},
+  {id: 4, bgColor: '#A6FFA1'},
+];
---
+++ data/CustomizeCrown.js
@@ -0,0 +1,42 @@
+export const CROWNS = [
+  {
+    isActive: true,
+    level: 1,
+    crowns: [
+      require('../assets/image/gameIcons/set1/crown1.png'),
+      require('../assets/image/gameIcons/set1/crown2.png'),
+      require('../assets/image/gameIcons/set1/crown3.png'),
+      require('../assets/image/gameIcons/set1/crown4.png'),
+    ],
+  },
+  {
+    isActive: false,
+    level: 2,
+    crowns: [
+      require('../assets/image/gameIcons/set2/crown5.png'),
+      require('../assets/image/gameIcons/set2/crown6.png'),
+      require('../assets/image/gameIcons/set2/crown7.png'),
+      require('../assets/image/gameIcons/set2/crown8.png'),
+    ],
+  },
+  {
+    isActive: false,
+    level: 3,
+    crowns: [
+      require('../assets/image/gameIcons/set3/crown9.png'),
+      require('../assets/image/gameIcons/set3/crown10.png'),
+      require('../assets/image/gameIcons/set3/crown11.png'),
+      require('../assets/image/gameIcons/set3/crown12.png'),
+    ],
+  },
+  {
+    isActive: false,
+    level: 4,
+    crowns: [
+      require('../assets/image/gameIcons/set4/crown13.png'),
+      require('../assets/image/gameIcons/set4/crown14.png'),
+      require('../assets/image/gameIcons/set4/crown15.png'),
+      require('../assets/image/gameIcons/set4/crown16.png'),
+    ],
+  },
+];
