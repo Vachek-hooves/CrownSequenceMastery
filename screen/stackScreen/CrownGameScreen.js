5a4c9438fbacd9a2aae0e4df81d659017f5a537d
Vachek-hooves <vachek0602@gmail.com>
2025-01-04 14:03:48 +0200

    + game layout

--- screen/stackScreen/CrownGameScreen.js
+++ screen/stackScreen/CrownGameScreen.js
@@ -1,25 +1,11 @@
 import {StyleSheet, Text, View, ImageBackground} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
+import GameScreenLayout from '../../components/layout/GameScreenLayout';
 
 const CrownGameScreen = () => {
-  return (
-    <LinearGradient
-      colors={['rgba(0,0,0,0.9)', 'rgba(0,0,0,0.9)']}
-      style={styles.container}
-      start={{x: 0, y: 0}}
-      end={{x: 1, y: 1}}>
-      
-    </LinearGradient>
-  );
+  return <GameScreenLayout></GameScreenLayout>;
 };
 
 export default CrownGameScreen;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(252, 248, 234, 0.5)',
-  },
-});
+const styles = StyleSheet.create({});
