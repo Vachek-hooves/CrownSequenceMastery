fa2faa9e523c4eb8ec28716cfa38ddf1a6f1b3ca
Vachek-hooves <vachek0602@gmail.com>
2025-01-09 16:41:43 +0200

    create screen

---
+++ screen/stackScreen/EditSequance.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const EditSequance = () => {
+  return (
+    <View>
+      <Text>EditSequance</Text>
+    </View>
+  )
+}
+
+export default EditSequance
+
+const styles = StyleSheet.create({})
